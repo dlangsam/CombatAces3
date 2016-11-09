40f2bc41a7617a2098b9c109c2ee2e00348af464
Devorah Langsam <devorah@langsam.org>
2016-11-09 16:27:22 -0500

    Create angular app with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name combatAcesApp
+ * @description
+ * # combatAcesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('combatAcesApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name combatAcesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the combatAcesApp
+ */
+angular.module('combatAcesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
